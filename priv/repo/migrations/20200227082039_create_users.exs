3aad53b8a0503e21c9cdc846b62f4b7e415038e0
Infinical <inficalvin@gmail.com>
2020-02-27 11:27:49 +0300

    Setup for user actions

---
+++ priv/repo/migrations/20200227082039_create_users.exs
@@ -0,0 +1,12 @@
+defmodule BuudaaApi.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string
+      add :username, :string, null: false
+    end
+
+    create unique_index(:users, [:username])
+  end
+end
